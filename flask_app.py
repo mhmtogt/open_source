e182e8711f1ba0933e7bdcc03c78cc8094d61ee9
Mehmet OGUT <mogut44949@gmail.com>
2023-12-05 22:26:42 +0300

    güvenlik

---
+++ flask_app.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource, reqparse
+import pandas as pd
+
+app = Flask(__name__)
+api = Api(app)
+
+def authenticate(username, password):
+    return username == 'admin' and password == 'password'
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not authenticate(auth.username, auth.password):
+            return jsonify({'message': 'Unauthorized access'}), 401
+        return f(*args, **kwargs)
+    return decorated
+
+class Users(Resource):
+    @requires_auth
+    def get(self):
+        data = pd.read_csv('users.csv')
+        data = data.to_dict('records')
+        return {'data' : data}, 200
+
+    @requires_auth
+    def post(self):
+        json = request.get_json()
+        req_data = pd.DataFrame({
+            'name'      : [json['name']],
+            'age'       : [json['age']],
+            'city'      : [json['city']]
+        })
+        data = pd.read_csv('users.csv')
+        data = pd.concat([data, req_data], ignore_index=True)
+        data.to_csv('users.csv', index=False)
+        return {'message' : 'Record successfully added.'}, 200
+
+    @requires_auth
+    def delete(self):
+        name = request.args['name']
+        data = pd.read_csv('users.csv')
+
+        if name in data['name'].values:
+            data = data[data['name'] != name]
+            data.to_csv('users.csv', index=False)
+            return {'message': 'Record successfully deleted.'}, 200
+        else:
+            return {'message': 'Record not found.'}, 404
+
+class Cities(Resource):
+    @requires_auth
+    def get(self):
+        data = pd.read_csv('users.csv', usecols=[2])
+        data = data.to_dict('records')
+        return {'data' : data}, 200
+
+class Name(Resource):
+    @requires_auth
+    def get(self,name):
+        data = pd.read_csv('users.csv')
+        data = data.to_dict('records')
+        for entry in data:
+            if entry['name'] == name :
+                return {'data' : entry}, 200
+        return {'message' : 'No entry found with this name !'}, 404
+
+api.add_resource(Users, '/users')
+api.add_resource(Cities, '/cities')
+api.add_resource(Name, '/<string:name>')
+
+if __name__ == '__main__':
+    app.run()
+
